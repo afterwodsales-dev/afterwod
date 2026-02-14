8ea9d643941fa3aca4f057556b0e7694b74c7b87
Gualguanosky <afterwod.sales@gmail.com>
2026-02-13 22:52:42 -0500

    deploy

--- web-port/src/App.jsx
+++ web-port/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { StoreProvider } from './context/StoreContext';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
